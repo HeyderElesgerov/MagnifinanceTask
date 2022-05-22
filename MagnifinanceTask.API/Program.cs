58d379599ecdbb741306037a4d0f33f64f84ca87
heyder <elesgerovheyder@gmail.com>
2022-05-22 13:38:49 +0400

    Call extension methods to inject and seed the database

--- MagnifinanceTask.API/Program.cs
+++ MagnifinanceTask.API/Program.cs
@@ -1,11 +1,22 @@
+using MagnifinanceTask.Infrastructure.Ioc;
+using MagnifinanceTask.Infrastructure.Seeds;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .InjectSqlServer(builder.Configuration.GetConnectionString("Local"))
+    .InjectRepositories()
+    .InjectServices()
+    .InjectAutoMapper()
+    .InjectSerilog(Path.Combine(Environment.CurrentDirectory, "app.log"));
+
+builder.Services.BuildServiceProvider().MigrateDatabase();
+builder.Services.BuildServiceProvider().SeedData();
 
 var app = builder.Build();
 
